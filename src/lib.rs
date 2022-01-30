e856669b12d343996362f912daba6ba1a474ce64
Harry <hello@majored.pw>
2022-01-30 13:19:55 +0000

    Implement Copy for Compression

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub mod write;
 use error::{Result, ZipError};
 
 /// A compression method supported by this crate.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub enum Compression {
     Stored,
     Deflate,
