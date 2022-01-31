b87e97618d9482a9b6ac5a11ec6997caaddfffbe
Harry <hello@majored.pw>
2022-01-31 14:36:14 +0000

    Addition of basic OffsetAsyncWriter doc comments (minimal as it's pub(crate))

--- src/write/offset_writer.rs
+++ src/write/offset_writer.rs
@@ -7,21 +7,27 @@ use std::task::{Poll, Context};
 
 use tokio::io::{AsyncWrite};
 
-// A writer which tracks the current byte offset.
+/// An async writer wrapper which tracks the current byte offset.
+/// 
+/// This type is only ever used internally to track offsets needed for central directory headers, and to easily
+/// calculate compressed & uncompressed file sizes.
 pub struct OffsetAsyncWriter<W: AsyncWrite + Unpin> {
     writer: W,
     offset: usize,
 }
 
 impl<W: AsyncWrite + Unpin> OffsetAsyncWriter<W> {
+    /// Constructs a new offset writer from a generic writer implementing AsyncWrite.
     pub fn from_raw(writer: W) -> Self {
         Self { writer, offset: 0 }
     }
 
+    /// Returns the current writer byte offset.
     pub fn offset(&self) -> usize {
         self.offset
     }
 
+    /// Consumes this writer and returns its inner value.
     pub fn into_inner(self) -> W {
         self.writer
     }
