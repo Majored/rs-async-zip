ab9af0eaea91f46a2133d4ceb8c95a700701b3f4
Harry <hello@majored.pw>
2022-02-06 15:13:35 +0000

    Addition of spec comment for date submodule

--- src/spec/date.rs
+++ src/spec/date.rs
@@ -3,11 +3,19 @@
 
 use chrono::{DateTime, Datelike, TimeZone, Timelike, Utc};
 
-/// Converts a date and time stored within ZIP headers into a `chrono` structure.
-///
-/// Spec notes:
-/// 'The date and time are encoded in standard MS-DOS format.  MS-DOS time format is different from more commonly used
-/// computer time formats such as UTC. For example, MS-DOS uses year values relative to 1980 and 2 second precision.'
+//  4.4.6 date and time fields: (2 bytes each)
+// 
+// The date and time are encoded in standard MS-DOS format.
+// If input came from standard input, the date and time are
+// those at which compression was started for this data. 
+// If encrypting the central directory and general purpose bit 
+// flag 13 is set indicating masking, the value stored in the 
+// Local Header will be zero. MS-DOS time format is different
+// from more commonly used computer time formats such as 
+// UTC. For example, MS-DOS uses year values relative to 1980
+// and 2 second precision.
+
+// Converts a date and time stored within ZIP headers into a `chrono` structure.
 pub fn zip_date_to_chrono(date: u16, time: u16) -> DateTime<Utc> {
     let years = (((date & 0xFE00) >> 9) + 1980).into();
     let months = ((date & 0x1E0) >> 5).into();
@@ -20,6 +28,7 @@ pub fn zip_date_to_chrono(date: u16, time: u16) -> DateTime<Utc> {
     Utc.ymd(years, months, days).and_hms(hours, mins, secs)
 }
 
+// Converts a `chrono` structure into a date and time stored in ZIP headers.
 pub fn chrono_to_zip_time(dt: &DateTime<Utc>) -> (u16, u16) {
     let year: u16 = (((dt.date().year() - 1980) << 9) & 0xFE00).try_into().unwrap();
     let month: u16 = ((dt.date().month() << 5) & 0x1E0).try_into().unwrap();
