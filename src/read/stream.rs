d4f21243df75351b270bf4f8d2345ff83129e027
Harry <hello@majored.pw>
2023-01-11 15:52:02 +0000

    Addition of module-level comments for the stream reader

--- src/read/stream.rs
+++ src/read/stream.rs
@@ -1,6 +1,50 @@
 // Copyright (c) 2023 Harry [Majored] [hello@majored.pw]
 // MIT License (https://github.com/Majored/rs-async-zip/blob/main/LICENSE)
 
+//! A ZIP reader which acts over a non-seekable source.
+//!
+//! # API Design
+//! As opposed to other readers provided by this crate, it's important that the data of an entry is fully read before
+//! the proceeding entry is read. This is as a result of not being able to seek forwards or backwards, so we must end
+//! up at the start of the next entry.
+//!
+//! **We encode this invariant within Rust's type system so that it can be enforced at compile time.**
+//!
+//! This requires that any transition methods between these encoded types consume the reader and provide a new owned
+//! reader back. This is certainly something to keep in mind when working with this reader, but idiomatic code can
+//! still be produced nevertheless.
+//!
+//! # Considerations
+//! As the central directory of a ZIP archive is stored at the end of it, a non-seekable reader doesn't have access
+//! to it. We have to rely on information provided within the local file header which may not be accurate or complete.
+//! This results in:
+//! - No file comment being avaliable (defaults to an empty string).
+//! - No internal or external file attributes being avaliable (defaults to 0).
+//! - The extra field data potentially being inconsistent with what's stored in the central directory.
+//! - None of the following being avaliable when the entry was written with a data descriptor (defaults to 0):
+//!     - CRC
+//!     - compressed size
+//!     - uncompressed size
+//!
+//! # Example
+//! ```no_run
+//! # use std::io::Cursor;
+//! # use async_zip::error::Result;
+//! # use async_zip::read::stream::ZipFileReader;
+//! #
+//! # async fn run() -> Result<()> {
+//! let mut zip = ZipFileReader::new(Cursor::new([0; 0]));
+//!     
+//! // Print the name of every file in a ZIP archive.
+//! while let Some(entry) = zip.next_entry().await? {
+//!     println!("File: {}", entry.entry().filename());
+//!     zip = entry.skip().await?;
+//! }
+//! #
+//! #     Ok(())
+//! # }
+//! ```
+
 use crate::error::Result;
 use crate::error::ZipError;
 use crate::spec::header::LocalFileHeader;
@@ -11,8 +55,8 @@ use tokio::io::{AsyncRead, BufReader};
 
 use super::seek::ZipEntryReader;
 
-struct Ready<R>(R);
-struct Reading<'a, R>(ZipEntryReader<'a, R>, ZipEntry);
+pub struct Ready<R>(R);
+pub struct Reading<'a, R>(ZipEntryReader<'a, R>, ZipEntry);
 
 /// A ZIP reader which acts over a non-seekable source.
 ///
