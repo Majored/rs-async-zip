0f40b5eec5fbcdcc684875ed9485fb1e990780d7
sid <sidred@gmail.com>
2023-01-16 22:20:55 +0530

    fix test name

--- tests/compress_test.rs
+++ tests/compress_test.rs
@@ -4,20 +4,20 @@ mod common;
 
 #[cfg(feature = "zstd")]
 #[tokio::test]
-async fn zip_compress_zstd_zip() {
+async fn zip_zstd_in_out() {
     let zip_data = common::compress_to_mem(Compression::Zstd).await;
     common::check_decompress_mem(zip_data).await
 }
 
 #[cfg(feature = "deflate")]
 #[tokio::test]
-async fn decompress_deflate_zip() {
+async fn zip_decompress_in_out() {
     let zip_data = common::compress_to_mem(Compression::Deflate).await;
     common::check_decompress_mem(zip_data).await
 }
 
 #[tokio::test]
-async fn decompress_store_zip() {
+async fn zip_store_in_out() {
     let zip_data = common::compress_to_mem(Compression::Stored).await;
     common::check_decompress_mem(zip_data).await
 }
