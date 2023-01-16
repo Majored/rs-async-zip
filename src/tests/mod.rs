cfc67b291d69b3b0550c8051df9fd8dcf95b2e7c
Skye Im <skye.im@cognite.com>
2023-01-16 13:12:03 +0100

    Tests: Only init env_logger once

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -5,3 +5,12 @@ pub(crate) mod combined;
 pub(crate) mod read;
 pub(crate) mod spec;
 pub(crate) mod write;
+
+use std::sync::Once;
+static ENV_LOGGER: Once = Once::new();
+
+/// Initialize the env logger for any tests that require it.
+/// Safe to call multiple times.
+fn init_logger() {
+    ENV_LOGGER.call_once(|| env_logger::init());
+}
