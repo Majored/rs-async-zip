4c5e8eb815e9ec7ee24d80b226e8cf72af752579
startewho <hoolooday@live.com>
2023-11-14 13:58:53 +0800

    fomart

--- src/entry/mod.rs
+++ src/entry/mod.rs
@@ -204,7 +204,7 @@ impl StoredZipEntry {
 
         // Found zip64.zip's CDS and LFH 's Extra field length not equal
         let mut record_ignore = self.header_size - 30;
-        let locol_ignore =header.file_name_length+header.extra_field_length;
+        let locol_ignore = header.file_name_length + header.extra_field_length;
 
         if record_ignore != locol_ignore {
             record_ignore = locol_ignore;
